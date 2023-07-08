664630ec463953ada985b182ed66dbce92d74bac
garymast <>
2023-07-08 19:47:23 +0000

    Add the initial user input function

--- run.py
+++ run.py
@@ -18,5 +18,44 @@ stats = SHEET.worksheet('stats')
 data = stats.get_all_values()
 
 print(data)
-f = Figlet(font='slant')
-print(f.renderText('EPL MATCH PREDICTOR'))
+
+
+def get_teams():
+    """
+    Get home and away teams input from the user.
+    Run a while loop to collect two valid teams from the user
+    via the terminal, which must be strings matching the specified teams
+    The loop will repeatedly request data, until it is valid.
+    """
+    
+    while True:
+        print("Enter the two teams, seperated by a comma:")
+        # print("Data should be six numbers, separated by commas.")
+        print("Example: Arsenal, Aston Villa\n")
+
+        data_str = input("Enter the teams:\n")
+        teams = data_str.split(",")
+        # if validate_data(teams):
+        #     print("Teams are valid!")
+        #     break
+        break
+    return teams
+
+
+def startText():
+    f = Figlet(font='slant')
+    print(f.renderText('EPL MATCH PREDICTOR'))
+    print("Welcome to EPL Match Predictor \n")
+    print("Delivering profitable football predictions since 2023 \n")
+    print("Enter opposing teams to receive a guaranteed* prediction \n")
+    print("Available teams are:\nArsenal, Aston Villa, Bournemouth, Brentford, Brighton,\nChelsea, Crystal Palace, Everton, Fulham, Leeds,\nLeicester, Liverpool, Man City, Man United, Newcastle,\nNottm Forest, Southampton, Tottenham, West Ham, Wolves")
+
+
+def main():
+    startText()
+    teams = get_teams()
+    print(teams)
+
+
+main()
+
